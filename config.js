396d1c0479c49c427def876b49ee53f7d069ccbc
ABHEESHABI <113508355+ABHEESHABI@users.noreply.github.com>
2022-10-12 19:51:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ module.exports = {
   VERSION: 'v1.0.0',
   BRANCH: "main",
   IMGBB_KEY: ["76a050f031972d9f27e329d767dd988f","deb80cd12ababea1c9b9a8ad6ce3fab2","78c84c62b32a88e86daf87dd509a657a"],
-  ALIVE: process.env.ALIVE || "https://telegra.ph/file/47842cf7d85784cb4e441.jpg Hey {sender}, I'm alive \n Uptime: {uptime}",
-  Session_Id: process.env.SESSION_ID || "bkxkWlhCSmU=",
+  ALIVE: process.env.ALIVE || "https://i.imgur.com/rN6vG0V.jpeg Hey {sender}, I'm alive \n Uptime: {uptime}",
+  Session_Id: process.env.SESSION_ID || "R0hQalJ3Ulo=",
   ANTILINK: toBool(process.env.ANTI_LINK) || false,
   ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
   LANG: process.env.LANG || "EN",
@@ -18,19 +18,20 @@ module.exports = {
       ? "^"
       : "^[.]",
   RMBG_KEY: process.env.RMBG_KEY || false,
-  PACKNAME: process.env.PACKNAME || "Abu",
+  PACKNAME: process.env.PACKNAME || "ABHI BRO",
   WELCOME_MSG:
     process.env.WELCOME_MSG ||
     "{pp}Hi @user Welcome to @gname\nYou're our @count/513 Members ",
   GOODBYE_MSG: process.env.GOODBYE_MSG || "Hi @user It was Nice Seeing you",
   AUTHOR: process.env.AUTHOR || "Jsl",
   DATABASE: DATABASE_URL === './database.db' ? new Sequelize({ dialect: 'sqlite', storage: DATABASE_URL, logging: false }) : new Sequelize(DATABASE_URL, {dialect: 'postgres', ssl: true, protocol: 'postgres', dialectOptions: { native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false }),
-  BOT_INFO: process.env.BOT_INFO || 'Abu MD,Jsl,Abu SER,Abu MD,https://telegra.ph/file/47842cf7d85784cb4e441.jpg',
-  SUDO: process.env.SUDO || "917025994178",
+  BOT_INFO: process.env.BOT_INFO || 'ABHI MD,Jsl,ABHI BRO,ABHI MD,https://i.imgur.com/rN6vG0V.jpeg',
+  SUDO: process.env.SUDO || "918137093395",
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || " ",
-  HEROKU_API_KEY: process.env.HEROKU_API_KEY || " ",
-  IMAGE_URL: process.env.IMAGE_URL || "https://telegra.ph/file/47842cf7d85784cb4e441.jpg",
-  BOT_NAME: process.env.BOT_NAME || "Abu-MD",
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || ""5151d23e-59ec-4e5b-9ca9-b24e0463c73a"
+ https://dashboard.heroku.com/account#:~:text=5151d23e%2D59ec%2D4e5b%2D9ca9%2Db24e0463c73a",
+  IMAGE_URL: process.env.IMAGE_URL || "https://i.imgur.com/rN6vG0V.jpeg",
+  BOT_NAME: process.env.BOT_NAME || "ABHI BRO-MD",
   MODE: process.env.MODE || "public",
   LANGUAGE: process.env.LANGUAGE || 'english',
 };
